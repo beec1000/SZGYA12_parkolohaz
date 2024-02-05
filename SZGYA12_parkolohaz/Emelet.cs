cfac4e16f96539b5be4f00b5394b3581a32366a6
beec1000 <beec1000.minecraft@gmail.com>
2024-02-05 20:11:31 +0100

    Add project files.

---
+++ SZGYA12_parkolohaz/Emelet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA12_parkolohaz
+{
+    internal class Emelet
+    {
+        public int Szint { get; set; }
+        public string SzintNev { get; set; }
+        public List<int> Szektorok { get; set; }
+
+        public Emelet(int n, string s)
+        {
+            string[] v = s.Split("; ");
+            Szint = n;
+            SzintNev = v[0];
+            Szektorok = new List<int>();
+            for (int i = 1; i < v.Length; i++)
+            {
+                Szektorok.Add(int.Parse(v[i]));
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Szint}. {SzintNev}: {string.Join("; ", Szektorok)}";
+        }
+    }
+}
---
+++ SZGYA12_parkolohaz/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SZGYA12_parkolohaz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static string F8(List<Emelet> e) => e.MinBy(d => d.Szektorok.Sum()).SzintNev;
+
+            static IEnumerable<Emelet> F9(List<Emelet> e)
+            {
+                var x = e.Where(d => d.Szektorok.Contains(0));
+                return x;
+            }
+
+            static void Main(string[] args)
+            {
+                List<Emelet> emeletek = new List<Emelet>();
+                var sr = new StreamReader(@"..\..\..\src\parkolohaz.txt");
+                for (int i = 1; !sr.EndOfStream; i++)
+                {
+                    emeletek.Add(new Emelet(i, sr.ReadLine()));
+                }
+
+                foreach (var i in emeletek)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine("8. feladat");
+                Console.WriteLine($"A legkevesebb autó a {F8(emeletek)} szinten van.");
+
+                //Console.WriteLine("9. feladat");
+                //if (F9(emeletek) != null)
+                //{
+                //    Console.WriteLine($"A {}. szinten, a {}. szektorban nincs autó.");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Hiba 404!!! nincs ilyen");
+                //}
+            }
+        }
+    }
+}
